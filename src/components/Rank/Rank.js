7fd1be53f8cb335cc9102ceee41d194b745d1165
Martin Bruno <mb.martinbruno@gmail.com>
2021-09-14 19:00:16 +0200

    fix styles

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -1,16 +1,14 @@
-import React from 'react';
+import React from "react";
 
 const Rank = ({ name, entries }) => {
   return (
     <div>
-      <div className='white f3'>
-        {`${name}, your current entry count is...`}
+      <div className="white f3 b mb">
+        {`${name}, you already have successfully detected ${entries} faces!`}
       </div>
-      <div className='white f1'>
-        {entries}
-      </div>
+      <div className="white f3"></div>
     </div>
   );
-}
+};
 
 export default Rank;
